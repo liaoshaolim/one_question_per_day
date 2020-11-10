d354a789e6fc526d66190a4b32e5b7814a0e5037
liaoshaolim <366688603@qq.com>
2020-11-10 14:28:55 +0800

    + 二叉树的最大深度

--- leetcode_easy.py
+++ leetcode_easy.py
@@ -341,3 +341,12 @@ class Solution:
         if not root:
             return True
         return isSameTree(root.left, root.right)
+
+    # 104. 二叉树的最大深度
+    def maxDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        else:
+            left_height = self.maxDepth(root.left)
+            right_height = self.maxDepth(root.right)
+            return max(left_height, right_height) + 1
