9243087fa9609dd6eaca45096ee26f31f42427f2
liaoshaolim <366688603@qq.com>
2020-11-10 14:18:47 +0800

    + 对称二叉树

--- leetcode_easy.py
+++ leetcode_easy.py
@@ -321,3 +321,23 @@ class Solution:
             return False
         else:
             return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+    # 101. 对称二叉树
+    # 思路：树左侧跟树右侧是相等的数
+    def isSymmetric(self, root: TreeNode) -> bool:
+        def isSameTree(p: TreeNode, q: TreeNode) -> bool:
+            # 如果两个二叉树都为空，则两个二叉树相同
+            if not p and not q:
+                return True
+            # 如果两个二叉树中有且只有一个为空，则两个二叉树一定不相同
+            elif not p or not q:
+                return False
+            # 如果两个二叉树都不为空，那么首先判断它们的根节点的值是否相同，若不相同则两个二叉树一定不同，若相同，再分别判断两个二叉树的左子树是否相同以及右子树是否相同
+            elif p.val != q.val:
+                return False
+            else:
+                # DETAIL： 与 相同的树 不同的地方：递归时，传参不同
+                return isSameTree(p.left, q.right) and isSameTree(p.right, q.left)
+        if not root:
+            return True
+        return isSameTree(root.left, root.right)
